7f8a1b60e258822ce4c1942d27bda062a7fca32c
Jike0106 <61101899+Jike0106@users.noreply.github.com>
2020-03-02 12:10:15 +0800

    IsPrime

---
+++ IsPrime.cpp
@@ -0,0 +1,24 @@
+#include  <iostream> 
+#include  <cmath> 
+using  namespace  std;
+int isPrime(int a) {
+    int i(3),flag(1);
+    if (a>2)
+	    if ( a % 2)
+	        for (; i < sqrt(a); i += 2)
+	            flag = a % i;
+	    else flag = 0;
+	else flag = 1; 
+    return flag;
+}
+
+int  main()
+{
+    unsigned  short  a;
+    cin >> a;
+    if (isPrime(a))
+        cout << "Y" << endl;
+    else
+        cout << "N" << endl;
+}
+
